1207203fa2fc738def1720e026228beb590419c6
Joan Garvin Cardona <joangarvincardona@gmail.com>
2026-05-15 16:15:37 +0200

    Detailed issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { Layout } from './components/Layout';
 import { IssueList } from './pages/IssueList';
+import { IssueDetail } from './pages/IssueDetail';
 
 /**
  * Component arrel de l'aplicació.
- * Aquí configurem els proveïdors de context globals.
+ * Aquí configurem els proveïdors de context globals i l'enrutament.
  */
 const App: React.FC = () => {
   return (
-    // Proveïdor d'autenticació per tenir l'usuari seleccionat a tota l'app
-    <AuthProvider>
-      {/* Estructura base de la web */}
-      <Layout>
-        {/* Pàgina de llistat d'incidències */}
-        <IssueList />
-      </Layout>
-    </AuthProvider>
+    <BrowserRouter>
+      {/* Proveïdor d'autenticació per tenir l'usuari seleccionat a tota l'app */}
+      <AuthProvider>
+        {/* Estructura base de la web */}
+        <Layout>
+          <Routes>
+            {/* Pàgina de llistat d'incidències */}
+            <Route path="/" element={<IssueList />} />
+            {/* Pàgina de detall d'incidència */}
+            <Route path="/issues/:id" element={<IssueDetail />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
+    </BrowserRouter>
   );
 };
 
 export default App;
+

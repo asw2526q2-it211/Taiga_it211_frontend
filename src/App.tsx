11c63b35913d809c9c9938c924e6629e867857f3
yimin-j <yimin.jin@estudiantat.upc.edu>
2026-05-18 11:54:39 +0200

    Refactoring para cambiar temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 import { Layout } from './components/Layout';
 import { IssueList } from './pages/IssueList';
 import { IssueDetail } from './pages/IssueDetail';
@@ -15,26 +16,28 @@ import { BulkInsert } from './pages/BulkInsert';
  */
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            {/* Pàgina de llistat d'incidències */}
-            <Route path="/" element={<IssueList />} />
-            {/* Pàgina de creació d'incidència */}
-            <Route path="/issues/new" element={<NewIssue />} />
-            {/* Inserció massiva d'incidències */}
-            <Route path="/issues/bulk" element={<BulkInsert />} />
-            {/* Pàgina de detall d'incidència */}
-            <Route path="/issues/:id" element={<IssueDetail />} />
-            {/* Pàgina de configuració */}
-            <Route path="/settings" element={<SettingsPage />} />
-            {/* Pàgina del perfil d'un usuari */}
-            <Route path="/profile/:username" element={<ProfilePage />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              {/* Pàgina de llistat d'incidències */}
+              <Route path="/" element={<IssueList />} />
+              {/* Pàgina de creació d'incidència */}
+              <Route path="/issues/new" element={<NewIssue />} />
+              {/* Inserció massiva d'incidències */}
+              <Route path="/issues/bulk" element={<BulkInsert />} />
+              {/* Pàgina de detall d'incidència */}
+              <Route path="/issues/:id" element={<IssueDetail />} />
+              {/* Pàgina de configuració */}
+              <Route path="/settings" element={<SettingsPage />} />
+              {/* Pàgina del perfil d'un usuari */}
+              <Route path="/profile/:username" element={<ProfilePage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+/* ─── Tema: carreguem els CSS de temes abans que els estils globals ─── */
+import './styles/themes/light.css'
+import './styles/themes/dark.css'
 import './index.css'
 import App from './App'
+import { initTheme } from './styles/theme'
+
+// Inicialitzar el tema desat (data-theme a <html>)
+initTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
